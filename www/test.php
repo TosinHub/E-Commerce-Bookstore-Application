554d0651e8c116f130e3e12074438d0f8e96661a
Tosin <tosinolugbenga@gmail.com>
2017-03-28 11:23:49 +0100

    set vebrose error mode

--- www/test.php
+++ www/test.php
@@ -4,7 +4,22 @@ define('DBNAME','bookstore');
 define('DBUSER','root');
 define('DBPASS','papa2657');
 
+
+
+try{
+
+#Prepare a PDO instance
 $conn = new PDO('mysql:host = localhost;dbname='.DBNAME, DBUSER, DBPASS);
 
+#set verbose error modes
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRORMODE_SILENT);
+
+
+}
+catch(PDOException $e){
+
+
+  echo |$e->getMessage();
+}
 
 
