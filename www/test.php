9fc80ba4cfdb719a63c368d7185f5abefd8422b3
Tosin <tosinolugbenga@gmail.com>
2017-03-28 11:31:19 +0100

    set vebrose error mode

--- www/test.php
+++ www/test.php
@@ -12,14 +12,14 @@ try{
 $conn = new PDO('mysql:host = localhost;dbname='.DBNAME, DBUSER, DBPASS);
 
 #set verbose error modes
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRORMODE_SILENT);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
 
 }
 catch(PDOException $e){
 
 
-  echo |$e->getMessage();
+  echo $e->getMessage();
 }
 
 
